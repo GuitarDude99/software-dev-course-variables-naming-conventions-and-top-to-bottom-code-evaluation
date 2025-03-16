1fd5acde61aa9573facb934f166e8b91b087d03c
Mike <007mrice@gmail.com>
2025-03-15 22:47:45 -0500

    Finalized script.js with discount implementation

---
+++ script#2.js
@@ -0,0 +1,17 @@
+// Customer details
+let name = "Jack";
+let quantity = 5;
+let price = 20;
+let itemType = "books"; 
+
+// Discount details
+let discountRate = 0.10; // 10% discount
+let discountAmount = price * discountRate; 
+let discountedPrice = price - discountAmount; 
+let totalCost = discountedPrice * quantity; 
+
+// Summary message
+console.log(`${name} bought ${quantity} ${itemType} for a total of $${totalCost.toFixed(2)} (after discount).`);
+
+
+
